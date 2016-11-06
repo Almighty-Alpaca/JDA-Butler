44d092b99e64d27fb0d37b252cd0940798a3deea
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-11-06 18:52:21 +0100

    Added Ping and Uptime commands

--- src/main/java/com/almightyalpaca/discord/jdabutler/EventListener.java
+++ src/main/java/com/almightyalpaca/discord/jdabutler/EventListener.java
@@ -1,5 +1,8 @@
 package com.almightyalpaca.discord.jdabutler;
 
+import java.lang.management.ManagementFactory;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -32,6 +35,8 @@ public class EventListener extends ListenerAdapter {
 												});
 
 	private static boolean			started;
+
+	private static final OffsetDateTime	startTime	= OffsetDateTime.now();
 
 	public static void start() {
 
@@ -455,6 +460,31 @@ public class EventListener extends ListenerAdapter {
 						+ ") to " + role.getName()), t -> Bot.LOG.log(t));
 			}
 
+		} else if (text.startsWith("!ping")) {
+			event.getChannel().sendMessage("Ping: ...").queue(m -> m.editMessage("Ping: " + event.getMessage().getCreationTime().until(m.getCreationTime(), ChronoUnit.MILLIS) + "ms").queue());
+		} else if (text.startsWith("!uptime")) {
+
+			final long duration = ManagementFactory.getRuntimeMXBean().getUptime();
+
+			final long years = duration / 31104000000L;
+			final long months = duration / 2592000000L % 12;
+			final long days = duration / 86400000L % 30;
+			final long hours = duration / 3600000L % 24;
+			final long minutes = duration / 60000L % 60;
+			final long seconds = duration / 1000L % 60;
+
+			String uptime = "";
+			uptime += years == 0 ? "" : years + " Year" + (years > 1 ? "s" : "") + ", ";
+			uptime += months == 0 ? "" : months + " Month" + (months > 1 ? "s" : "") + ", ";
+			uptime += days == 0 ? "" : days + " Day" + (days > 1 ? "s" : "") + ", ";
+			uptime += hours == 0 ? "" : hours + " Hour" + (hours > 1 ? "s" : "") + ", ";
+			uptime += minutes == 0 ? "" : minutes + " Minute" + (minutes > 1 ? "s" : "") + ", ";
+			uptime += seconds == 0 ? "" : seconds + " Second" + (seconds > 1 ? "s" : "") + ", ";
+
+			uptime = com.almightyalpaca.discord.jdabutler.util.StringUtils.replaceLast(uptime, ", ", "");
+			uptime = com.almightyalpaca.discord.jdabutler.util.StringUtils.replaceLast(uptime, ",", " and");
+
+			builder.appendString(uptime);
 		}
 
 		if (builder.getLength() > 0) {
