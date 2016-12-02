132d3d95a59b93da0094a13c23a036f260b1698d
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-12-02 22:31:54 +0100

    Added GradleUtil.getDependencyBlock(...)

--- src/main/java/com/almightyalpaca/discord/jdabutler/GradleUtil.java
+++ src/main/java/com/almightyalpaca/discord/jdabutler/GradleUtil.java
@@ -6,12 +6,18 @@ import org.apache.commons.lang3.tuple.Triple;
 
 public class GradleUtil {
 	public static String getBuildFile(final Collection<Triple<String, String, String>> dependencies, final boolean pretty) {
-		String text = "plugins {\r\n    id 'java'\r\n    id 'application'\r\n    id 'com.github.johnrengelman.shadow' version '1.2.4'\r\n}\r\n// Change this to the name of your main class\r\nmainClassName = 'com.example.jda.Bot'\r\n\r\n// This version will be appeneded to the jar name\r\nversion '1.0'\r\n\r\ngroup 'com.mydomain.example'\r\n\r\n// The java version\r\nsourceCompatibility = 1.8\r\ntargetCompatibility = 1.8\r\n\r\nrepositories {\r\n    mavenCentral()\r\n    jcenter()\r\n}\r\n\r\n// Add your dependencies here\r\ndependencies {\r\n";
-		for (final Triple<String, String, String> dependency : dependencies) {
-			text += "    " + GradleUtil.getDependencyString(dependency.getLeft(), dependency.getMiddle(), dependency.getRight(), pretty) + "'\r\n";
-		}
+		String text = "plugins {\n    id 'java'\n    id 'application'\n    id 'com.github.johnrengelman.shadow' version '1.2.4'\n}\n// Change this to the name of your main class\nmainClassName = 'com.example.jda.Bot'\n\n// This version will be appeneded to the jar name\nversion '1.0'\n\ngroup 'com.mydomain.example'\n\n// The java version\nsourceCompatibility = 1.8\ntargetCompatibility = 1.8\n\nrepositories {\n    mavenCentral()\n    jcenter()\n}\n\n// Add your dependencies here\n";
+		text += GradleUtil.getDependencyBlock(dependencies, pretty);
+		text += "\n\n// Tell gradle to use UTF-8 as file encoding\ncompileJava.options.encoding = 'UTF-8'\n\n// This task is to set the gradle wrapper version\ntask wrapper(type: Wrapper) {\n    gradleVersion = '3.2'\n}";
+		return text;
+	}
 
-		text += "}\r\n\r\n// Tell gradle to use UTF-8 as file encoding\r\ncompileJava.options.encoding = 'UTF-8'\r\n\r\n// This task is to set the gradle wrapper version\r\ntask wrapper(type: Wrapper) {\r\n    gradleVersion = '3.2'\r\n}";
+	public static String getDependencyBlock(final Collection<Triple<String, String, String>> dependencies, final boolean pretty) {
+		String text = "dependencies {\n";
+		for (final Triple<String, String, String> dependency : dependencies) {
+			text += "    " + GradleUtil.getDependencyString(dependency.getLeft(), dependency.getMiddle(), dependency.getRight(), pretty) + "\n";
+		}
+		text += "}";
 		return text;
 	}
 
