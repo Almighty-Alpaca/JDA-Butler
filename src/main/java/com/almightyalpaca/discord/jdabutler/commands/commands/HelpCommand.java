755b25bd7ce2b20e845ddefc431dadc9666ec23d
Almighty-Alpaca <Almighty-Alpaca@users.noreply.github.com>
2016-12-19 13:40:20 +0100

    Beautified help command

--- src/main/java/com/almightyalpaca/discord/jdabutler/commands/commands/HelpCommand.java
+++ src/main/java/com/almightyalpaca/discord/jdabutler/commands/commands/HelpCommand.java
@@ -2,6 +2,8 @@ package com.almightyalpaca.discord.jdabutler.commands.commands;
 
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.almightyalpaca.discord.jdabutler.Bot;
 import com.almightyalpaca.discord.jdabutler.EmbedUtil;
 import com.almightyalpaca.discord.jdabutler.commands.Command;
@@ -18,8 +20,13 @@ public class HelpCommand implements Command {
 	public void dispatch(final User sender, final TextChannel channel, final Message message, final String content, final GuildMessageReceivedEvent event) {
 		final EmbedBuilder builder = new EmbedBuilder();
 		EmbedUtil.setColor(builder);
-		final String help = Bot.dispatcher.getCommands().stream().filter(c -> c.getHelp() != null).map(c -> String.format("%s%s - %s", Bot.config.getString("prefix", ""), c.getName().toLowerCase(), c
-				.getHelp())).collect(Collectors.joining("\n"));
+
+		final String prefix = Bot.config.getString("prefix");
+
+		final int size = Bot.dispatcher.getCommands().stream().filter(c -> c.getHelp() != null).map(c -> c.getName().length()).max((i1, i2) -> i1.compareTo(i2)).get() + 1 + prefix.length();
+
+		final String help = Bot.dispatcher.getCommands().stream().filter(c -> c.getHelp() != null).map(c -> "`" + StringUtils.rightPad(prefix + c.getName().toLowerCase() + "", size, ".") + "` - " + c
+				.getHelp()).collect(Collectors.joining("\n"));
 		builder.setAuthor(channel.getGuild().getMember(sender).getEffectiveName(), null, sender.getEffectiveAvatarUrl());
 		builder.setDescription(help);
 		channel.sendMessage(new MessageBuilder().setEmbed(builder.build()).build()).queue();
