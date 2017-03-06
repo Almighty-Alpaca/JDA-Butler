2e0b731837567e915b6d4641853804865d9369b6
ArsenArsen <ArsenArsen@users.noreply.github.com>
2017-03-06 14:25:51 +0100

    Add AnnouncementsCommand.java

---
+++ src/main/java/com/almightyalpaca/discord/jdabutler/commands/commands/AnnouncementsCommand.java
@@ -0,0 +1,38 @@
+package com.almightyalpaca.discord.jdabutler.commands.commands;
+
+import com.almightyalpaca.discord.jdabutler.commands.Command;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+import java.util.List;
+
+public class AnnouncementsCommand implements Command {
+	@Override
+	public void dispatch(User sender, TextChannel channel, Message message, String content, GuildMessageReceivedEvent event) throws Exception {
+		if (content.toLowerCase().matches(".+ .+")) {
+			String[] args = content.split(" ", 2);
+			List<Role> roles = channel.getGuild().getRolesByName(args[0] + " updates", true);
+			if (roles.isEmpty()) {
+				channel.sendMessage("Found no roles to mention!").queue();
+			} else {
+				Role role = roles.get(0);
+				role.getManager().setMentionable(true)
+						.queue(s -> channel.sendMessage(role.getAsMention() + " " + args[1])
+								.queue(m -> role.getManager().setMentionable(false).queue()));
+			}
+		}
+	}
+
+	@Override
+	public String getHelp() {
+		return "`announce [type] [text]` - Mentions `@[type] Updates` role with the text following.";
+	}
+
+	@Override
+	public String getName() {
+		return "announce";
+	}
+}
