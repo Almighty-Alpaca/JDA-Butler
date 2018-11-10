59e89696b4432fc9c2b63fe5eff29a35cf8ce752
Michael Ritter <ritter.michael92@gmail.com>
2018-11-10 19:12:04 +0100

    Add ci buildNr to build command

--- bot/src/main/java/com/almightyalpaca/discord/jdabutler/BotVersion.java
+++ bot/src/main/java/com/almightyalpaca/discord/jdabutler/BotVersion.java
@@ -2,11 +2,14 @@ package com.almightyalpaca.discord.jdabutler;
 
 public class BotVersion
 {
-    public static final String FULL_VERSION = "@VERSION@";
+    public static final String BUILD = "@BUILD@";
+    public static final String GIT_INFO = "@VERSION@";
 
-    private static final String[] VERSION_LINES = FULL_VERSION.split("\n");
+    private static final String[] GIT_LINES = GIT_INFO.split("\n");
 
-    public static final String VERSION_REF = VERSION_LINES.length == 1 ? "" : VERSION_LINES[0];
-    public static final String VERSION_COMMIT = VERSION_LINES.length == 1 ? "" : VERSION_LINES[1];
-    public static final String VERSION_MSG = VERSION_LINES.length == 1 ? "" : VERSION_LINES[2];
+    public static final String GIT_REF = GIT_LINES.length == 1 ? "" : GIT_LINES[0];
+    public static final String GIT_COMMIT = GIT_LINES.length == 1 ? "" : GIT_LINES[1];
+    public static final String GIT_MSG = GIT_LINES.length == 1 ? "" : GIT_LINES[2];
+
+    public static final String FULL_VERSION = String.format("Build: %s\n\nGit:\n%s", BUILD, GIT_INFO);
 }
