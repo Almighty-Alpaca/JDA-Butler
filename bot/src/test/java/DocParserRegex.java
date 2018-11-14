0a95acb7e8e23d34e2ce203d4779ef8526285ff8
Michael Ritter <ritter.michael92@gmail.com>
2018-11-14 14:37:55 +0100

    Fix and extend tests for JDoc

--- bot/src/test/java/DocParserRegex.java
+++ bot/src/test/java/DocParserRegex.java
@@ -11,37 +11,45 @@ public class DocParserRegex {
     public void testMethodNormal() {
         Matcher matcher = JDocParser.METHOD_PATTERN.matcher("void funcName(String a, int b)");
         assertTrue("Matcher can't find normal methods", matcher.find());
-        assertEquals("Matcher doesn't extract function-return correctly", "void", matcher.group(1));
-        assertEquals("Matcher doesn't extract function-name correctly", "funcName", matcher.group(2));
-        assertEquals("Matcher doesn't extract function-params correctly", "String a, int b", matcher.group(3));
+        assertNull("Matcher doesn't extract annos/modifiers correctly", matcher.group(1));
+        assertEquals("Matcher doesn't extract function-return correctly", "void", matcher.group(2));
+        assertEquals("Matcher doesn't extract function-name correctly", "funcName", matcher.group(3));
+        assertEquals("Matcher doesn't extract function-params correctly", "String a, int b", matcher.group(4));
+    }
+
+    @Test
+    public void testMethodAnnosAndModifiers() {
+        Matcher matcher = JDocParser.METHOD_PATTERN.matcher("@Deprecated\n@DeprecatedSince(\"testing happened\")\npublic static void funcName(String a, int b)");
+        assertTrue("Matcher can't find methods with annos/modifiers", matcher.find());
+        assertEquals("Matcher doesn't extract annos/modifiers correctly", "@Deprecated\n@DeprecatedSince(\"testing happened\")\npublic static ", matcher.group(1));
     }
 
     @Test
     public void testMethodDots() {
         Matcher matcher = JDocParser.METHOD_PATTERN.matcher("java.lang.String funcName(java.lang.String a)");
         assertTrue("Matcher can't find methods with dots", matcher.find());
-        assertEquals("Matcher doesn't extract function-args with dots correctly", "java.lang.String a", matcher.group(3));
+        assertEquals("Matcher doesn't extract function-args with dots correctly", "java.lang.String a", matcher.group(4));
     }
 
     @Test
     public void testMethodGenerics() {
         Matcher matcher = JDocParser.METHOD_PATTERN.matcher("java.lang.String funcName(Collection<String> a)");
         assertTrue("Matcher can't find methods with generics", matcher.find());
-        assertEquals("Matcher doesn't extract function-args with generics correctly", "Collection<String> a", matcher.group(3));
+        assertEquals("Matcher doesn't extract function-args with generics correctly", "Collection<String> a", matcher.group(4));
     }
 
     @Test
     public void testMethodArrays() {
         Matcher matcher = JDocParser.METHOD_PATTERN.matcher("void funcName(byte[] a)");
         assertTrue("Matcher can't find methods with arrs", matcher.find());
-        assertEquals("Matcher doesn't extract function-args with arrs correctly", "byte[] a", matcher.group(3));
+        assertEquals("Matcher doesn't extract function-args with arrs correctly", "byte[] a", matcher.group(4));
     }
 
     @Test
     public void testMethodCombined() {
         Matcher matcher = JDocParser.METHOD_PATTERN.matcher("java.lang.String getInviteUrl(java.util.Collection<Permission> permissions, byte[] arr)");
         assertTrue("Matcher can't find methods with generics + dots + arrs", matcher.find());
-        assertEquals("Matcher doesn't extract function-args with generics and correctly", "java.util.Collection<Permission> permissions, byte[] arr", matcher.group(3));
+        assertEquals("Matcher doesn't extract function-args with generics, dots & arrs correctly", "java.util.Collection<Permission> permissions, byte[] arr", matcher.group(4));
     }
 
     @Test
--- bot/src/test/java/JDocParserTest.java
+++ bot/src/test/java/JDocParserTest.java
@@ -2,6 +2,7 @@ import com.almightyalpaca.discord.jdabutler.Bot;
 import com.kantenkugel.discordbot.jdocparser.Documentation;
 import com.kantenkugel.discordbot.jdocparser.JDoc;
 import com.kantenkugel.discordbot.jdocparser.JDocUtil;
+import net.dv8tion.jda.core.AccountType;
 import okhttp3.OkHttpClient;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,8 +34,27 @@ public class JDocParserTest {
         List<Documentation> docs = JDoc.get("messagechannel.getiterablehistory");
         assertEquals("MessageChannel.getIterableHistory should be found", 1, docs.size());
         Documentation doc = docs.get(0);
-        assertTrue("Embed should have Return field", doc.getFields().keySet().stream().anyMatch(f -> f.equals("Returns:")));
-        assertTrue("Embed should have Throws field", doc.getFields().keySet().stream().anyMatch(f -> f.equals("Throws:")));
+        assertTrue("Embed should have Return field", doc.getFields().containsKey("Returns:"));
+        assertTrue("Embed should have Throws field", doc.getFields().containsKey("Throws:"));
+    }
+
+    @Test
+    public void getMethodWithDeprecation() { //will eventually be needed to be swapped out (deprecations get removed)
+        List<Documentation> docs = JDoc.get("jdabuilder.buildasync");
+        assertEquals("JDABuilder#buildAsync() should be found", 1, docs.size());
+        Documentation doc = docs.get(0);
+        assertTrue("Embed should have Deprecated field", doc.getFields().containsKey("Deprecated:"));
+        assertTrue("Embed title should contain Deprecated(Since)", doc.getTitle().contains("@Deprecated(Since"));
+        assertTrue("Embed title should contain ReplaceWith", doc.getTitle().contains("@ReplaceWith("));
+    }
+
+    @Test
+    public void getMethodWithIncubating() { //will eventually be needed to be swapped out (incubation status changes)
+        List<Documentation> docs = JDoc.get("game.watching");
+        assertEquals("Game.watching should be found", 1, docs.size());
+        Documentation doc = docs.get(0);
+        assertTrue("Embed should have Incubating field", doc.getFields().containsKey("Incubating:"));
+        assertTrue("Embed title should contain Incubating", doc.getTitle().contains("@Incubating"));
     }
 
     @Test
@@ -42,7 +62,20 @@ public class JDocParserTest {
         List<Documentation> docs = JDoc.get("jda.getUserById(long)");
         assertEquals("JDA.getUserById(long) should be found", 1, docs.size());
         Documentation doc = docs.get(0);
-        assertTrue("Embed should have Parameters field", doc.getFields().keySet().stream().anyMatch(f -> f.equals("Parameters:")));
+        assertTrue("Embed should have Parameters field", doc.getFields().containsKey("Parameters:"));
+    }
+
+    @Test
+    public void checkStaticHandling() {
+        List<Documentation> docs = JDoc.get("Game.playing");
+        assertEquals("Game.playing should be found", 1, docs.size());
+        Documentation doc = docs.get(0);
+        assertTrue("Game.of should be shown as static", doc.getShortTitle().startsWith("Game.playing("));
+
+        docs = JDoc.get("JDA#getUsersByName");
+        assertEquals("JDA#getUsersByName should be found", 1, docs.size());
+        doc = docs.get(0);
+        assertTrue("JDA#getUsersByName should be shown as non-static", doc.getShortTitle().startsWith("JDA#getUsersByName("));
     }
 
     @Test
@@ -50,7 +83,8 @@ public class JDocParserTest {
         List<Documentation> docs = JDoc.get("accounttype");
         assertEquals("AccountType should be found", 1, docs.size());
         Documentation doc = docs.get(0);
-        assertTrue("Embed should have Throws field", doc.getFields().keySet().stream().anyMatch(f -> f.equals("Values:")));
+        assertTrue("Embed should have Values field", doc.getFields().containsKey("Values:"));
+        assertEquals("AccountType value count mismatches", AccountType.values().length, doc.getFields().get("Values:").size());
     }
 
     @Test
@@ -82,7 +116,7 @@ public class JDocParserTest {
         List<Documentation> message = JDoc.get("Message");
         assertEquals("Message should be found as single result", 1, message.size());
         assertEquals("URL of Message docs mismatches",
-                "https://ci.dv8tion.net/job/JDA/lastSuccessfulBuild/javadoc/net/dv8tion/jda/core/entities/Message.html",
+                "https://ci.dv8tion.net/job/JDA/javadoc/net/dv8tion/jda/core/entities/Message.html",
                 message.get(0).getUrl(JDocUtil.JDOCBASE)
         );
     }
