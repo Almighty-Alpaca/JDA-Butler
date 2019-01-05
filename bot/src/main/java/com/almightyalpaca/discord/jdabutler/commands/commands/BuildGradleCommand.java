edddb8a0024210b3b480dd0b7676f26344b2f5e8
Michael Ritter <ritter.michael92@gmail.com>
2019-01-05 19:33:36 +0100

    Add missing change in BuildGradleCommand

--- bot/src/main/java/com/almightyalpaca/discord/jdabutler/commands/commands/BuildGradleCommand.java
+++ bot/src/main/java/com/almightyalpaca/discord/jdabutler/commands/commands/BuildGradleCommand.java
@@ -22,7 +22,7 @@ public class BuildGradleCommand extends Command
 
         List<VersionedItem> items = VersionCheckerRegistry.getItemsFromString(content, true).stream()
                 //only allow items which use maven for versioning
-                .filter(item -> item.getCustomVersionSupplier() == null)
+                .filter(item -> item.getGroupId() != null && item.getArtifactId() != null && item.getRepoType() != null)
                 .collect(Collectors.toList());
 
         final boolean pretty = content.contains("pretty");
