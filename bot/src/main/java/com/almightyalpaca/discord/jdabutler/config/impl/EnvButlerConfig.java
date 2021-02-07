d2ad910e9ef81507250883763df9c85ac4c33f05
duncte123 <contact@duncte123.me>
2021-02-07 10:48:27 +0100

    Implement persitence for versions

--- bot/src/main/java/com/almightyalpaca/discord/jdabutler/config/impl/EnvButlerConfig.java
+++ bot/src/main/java/com/almightyalpaca/discord/jdabutler/config/impl/EnvButlerConfig.java
@@ -1,12 +1,35 @@
 package com.almightyalpaca.discord.jdabutler.config.impl;
 
+import com.almightyalpaca.discord.jdabutler.Bot;
 import com.almightyalpaca.discord.jdabutler.config.ButlerConfig;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.lang.Boolean.parseBoolean;
 import static java.lang.Long.parseLong;
 import static java.lang.System.getenv;
 
 public class EnvButlerConfig implements ButlerConfig {
+
+    private static final String VERSION_BUILD_FILE = "jdaVersionBuildNum.txt";
+    private static final String VERSION_NAME_FILE = "jdaVersionName.txt";
+
+    private final AtomicInteger jdaVersionBuild;
+    private final AtomicReference<String> jdaVersionName;
+
+    public EnvButlerConfig() {
+        // init version objects
+        final String tmpVersionNum = readVersionFile(VERSION_BUILD_FILE, "-1");
+        final String tmpVersionName = readVersionFile(VERSION_NAME_FILE, "");
+
+        this.jdaVersionBuild = new AtomicInteger(Integer.parseInt(tmpVersionNum));
+        this.jdaVersionName = new AtomicReference<>(tmpVersionName);
+    }
 
     @Override
     public boolean testing() {
@@ -78,29 +101,58 @@ public class EnvButlerConfig implements ButlerConfig {
         return getenv("BUTLER_GITHUB_TOKEN");
     }
 
-    // will use file based storage
     @Override
     public int jdaVersionBuild() {
-        return 0;
+        return this.jdaVersionBuild.get();
     }
 
     @Override
     public String jdaVersionName() {
-        return null;
+        return this.jdaVersionName.get();
     }
 
     @Override
     public void setJDAVersionBuild(int newVersion) {
-
+        this.jdaVersionBuild.set(newVersion);
     }
 
     @Override
     public void setJDAVersionName(String newName) {
-
+        this.jdaVersionName.set(newName);
     }
 
     @Override
     public void save() {
+        writeVersionFile(VERSION_BUILD_FILE, this.jdaVersionBuild.toString()); // runs Integer.toString(get())
+        writeVersionFile(VERSION_NAME_FILE, this.jdaVersionName.get());
+    }
 
+    private String readVersionFile(String file, String defaultVal) {
+        try {
+            final File versionFile = new File(file);
+
+            if (!versionFile.exists()) {
+                versionFile.createNewFile();
+
+                writeVersionFile(file, defaultVal);
+
+                return defaultVal;
+            }
+
+            return String.join("", Files.readAllLines(versionFile.toPath())).trim();
+        } catch(IOException e) {
+            Bot.LOG.error("Version file operation failed", e);
+        }
+
+        return defaultVal;
+    }
+
+    // file must exist
+    private void writeVersionFile(String file, String value) {
+        try (final FileWriter writer = new FileWriter(file)) {
+            writer.write(value);
+        } catch(IOException e) {
+            Bot.LOG.error("Error writing to file", e);
+        }
     }
 }
